9952295d9297ab5ee0b630fb7945fbde7b3b4ef5
wHo69 <85286288+wHo69@users.noreply.github.com>
2022-01-20 10:13:02 +0800

    Updated digits

--- digits.cpp
+++ digits.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <sstream>
 
 int count(int n) {
     if (n == 1) {
         return 1;
     } else {
-        std::string x = n;
-        return count(n.length()) + 1;
+        std::string x;
+        std::stringstream ss;
+        ss << n;
+        ss >> x;
+        return count(x.length()) + 1;
     }
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::cout << count(n);
+    std::string n;
+    while (true) {
+        std::cin >> n;
+        if (n == "END") {
+            break;
+        }
+        std::cout << count(std::stoi(n));   
+    }
     return 0;
 }
