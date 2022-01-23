68844a97b0f7c9a0abc59c186be5cecf88b2aee8
wHo69 <hu_bowen@students.edu.sg>
2022-01-23 22:06:05 +0800

    Added shiritori

---
+++ shiritori.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n;
+	std::cin >> n;
+	std::string inputs[m];
+	
+	for (int i = 0; i < n; i++) {
+		std::cin >> inputs[i];
+		if (i != 0) {
+			// check if last char == first char
+			// check for repitition
+		}
+	}
+}
