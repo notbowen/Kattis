f44b86f2d3a33dc39f83c04a5f53024f889fc5e9
wHo69 <hu_bowen@students.edu.sg>
2022-01-27 17:46:25 +0800

    Added stararragements

--- completed/3rd Session Hw/climbingworm2.cpp
+++ completed/3rd Session Hw/climbingworm2.cpp
@@ -1,3 +1,4 @@
+// DOESNT WORK LMAO
 #include <bits/stdc++.h>
 
 int main() {
@@ -8,13 +9,19 @@ int main() {
     long long a, b, h;
     std::cin >> a >> b >> h;
 
-	if (a == h) {
+	if (a >= h) {
 		std::cout << 1;
 		return 0;
 	}
 
+	long long y = a - b;
+	if ((h - b) % (a - b) == 0) {
+		std::cout << (h - b) / (a - b);
+		return 0;
+	}
+
     long long x = a - b;
-    double output = static_cast<double>(h) /static_cast<double>(x);
+    long double output = static_cast<long double>(h) /static_cast<long double>(x);
 
     if (output != (long long)output) {
         output = ceil(output);
