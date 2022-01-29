f4fd4f3ad5b4a89ff9342f85eb69f82f25c350bc
wHo69 <hu_bowen@students.edu.sg>
2022-01-29 19:08:55 +0800

    Added a template cpp file

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+}
